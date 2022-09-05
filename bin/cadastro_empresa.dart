60cea97adfb84182d5c60103d9061cfd3a77a1eb
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-09-04 21:24:20 -0300

    trocando arquivo de entrada

--- bin/cadastro_empresa.dart
+++ bin/cadastro_empresa.dart
@@ -1,77 +1,11 @@
-import 'dart:io';
-
-import 'package:cadastro_empresa/repositorio_local.dart';
-import 'package:cadastro_empresa/empresa_model.dart';
-import 'package:cadastro_empresa/endereco_model.dart';
-import 'package:cadastro_empresa/pessoaFisica.dart';
-import 'package:cadastro_empresa/pessoaJuridica.dart';
-import 'package:uuid/uuid.dart';
+import 'package:cadastro_empresa/dao.dart';
+import 'package:cadastro_empresa/repositorio_padrao.dart';
+import 'package:cadastro_empresa/sistema.dart';
 
 void main() {
-  print('\n');
-  var enderecoLuan = Endereco(
-    logradouro: 'Avendia Rui Barbosa',
-    numero: 2053,
-    complemento: 'casa04',
-    bairo: 'Santana',
-    estado: 'São Paulo',
-    cep: '12211-105',
-  );
-
-  var luan = PessoaFisica(
-    nomeIdentificador: 'Luan Fonseca',
-    documento: '35214200862',
-    endereco: enderecoLuan,
-  );
-
-  var luanJuridico = PessoaJuridica(
-    nomeIdentificador: 'Luan Fonseca 35214200862',
-    nomeFantasia: 'MEI do Luan Fonseca',
-    documento: '40245842000128',
-    endereco: enderecoLuan,
-  );
-
-  var empresaLuan = Empresa(
-    id: Uuid().v1(),
-    criadoEm: DateTime.now(),
-    razaoSocial: 'Luan Fonseca Produções',
-    nomeFantasia: 'Luan Fonseca352',
-    cnpj: '40242842000128',
-    endereco: enderecoLuan,
-    telefone: '12997213383',
-    socio: luan,
-  );
-
-  var empresaLuan2 = Empresa(
-    id: Uuid().v1(),
-    criadoEm: DateTime.now(),
-    razaoSocial: 'Luan Gimenez Dança',
-    nomeFantasia: 'Dança em Video',
-    cnpj: '53478145000178',
-    endereco: enderecoLuan,
-    telefone: '12997213383',
-    socio: luanJuridico,
-  );
-
-  // print(empresaLuan);
-  // print('\n');
-  // print(empresaLuan2);
-
-  var dbPessoaFisica = File('./database/pessoa_fisica.json');
-  var dbPessoaJuridica = File('./database/pessoa_juridica.json');
-  var dbEmpresa = File('./database/empresa.json');
-
-  var daoPessoaFisica = RepositorioLocal(arquivo: dbPessoaFisica);
-  var daoPessoaJuridica = RepositorioLocal(arquivo: dbPessoaJuridica);
-  var daoEmpresa = RepositorioLocal(arquivo: dbEmpresa);
-
-  //daoPessoaFisica.encontrarTodos();
-  //daoPessoaJuridica.encontrarTodos();
-  daoEmpresa.encontrarTodos();
-  daoEmpresa.adicionar(empresaLuan2.toJson());
-  daoPessoaFisica.adicionar(luan.toJson());
-  daoPessoaJuridica.adicionar(luanJuridico.toJson());
-  //daoPessoaFisica.encontrarTodos();
-  //daoPessoaJuridica.encontrarTodos();
-  daoEmpresa.encontrarTodos();
+  // var dbEmpresa = File('../database/empresa.json');
+  // DAO repoLocalJson = RepositorioLocal(arquivo: dbEmpresa);
+  DAO repoLocal = RepositorioPadrao();
+  //Sistema(repo: repoLocalJson).inicializar();
+  Sistema(repo: repoLocal).inicializar();
 }
