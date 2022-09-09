93b82e85d8d1747856206512d0b028491330b53e
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-09-08 23:58:50 -0300

    refatora arquivo de entrada

--- bin/cadastro_empresa.dart
+++ bin/cadastro_empresa.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:cadastro_empresa/dao.dart';
+import 'package:cadastro_empresa/repositorio_local.dart';
 import 'package:cadastro_empresa/repositorio_padrao.dart';
+//import 'package:cadastro_empresa/repositorio_padrao.dart';
 import 'package:cadastro_empresa/sistema.dart';
 
 void main() {
+  //DAO repoLocal = RepositorioLocal(arquivo: File('./database/empresa.txt'));
   DAO repoLocal = RepositorioPadrao();
   Sistema(repo: repoLocal).inicializar();
 }
